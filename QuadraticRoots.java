844ee78c884d37509c022e2106a072e0bdb17e1e
Anish Khandelwal <anishkhandelwal21@gmail.com>
2020-06-10 15:51:03 +0530

    Add .java and .class file

---
+++ QuadraticRoots.java
@@ -0,0 +1,27 @@
+class Quadratic{
+    public static void main(String [] ak){
+        double a = 2.3, b = 4, c = 5.6;
+        double root1, root2;
+        double determinant = b * b - 4 * a * c;
+
+        // condition for real and different roots
+        if(determinant > 0) {
+            root1 = (-b + Math.sqrt(determinant)) / (2 * a);
+            root2 = (-b - Math.sqrt(determinant)) / (2 * a);
+            System.out.format("\nroot1 = %.2f and root2 = %.2f\n", root1 , root2);
+        }
+        // Condition for real and equal roots
+        else if(determinant == 0) {
+            root1 = root2 = -b / (2 * a);
+            System.out.format("\nroot1 = root2 = %.2f\n", root1);
+        }
+        // If roots are not real
+        else {
+            double realPart = -b / (2 *a);
+            double imaginaryPart = Math.sqrt(-determinant) / (2 * a);
+
+            System.out.format("\nroot1 = %.2f+%.2fi and root2 = %.2f-%.2fi\n", 
+                              realPart, imaginaryPart, realPart, imaginaryPart);
+        }
+    }
+}
